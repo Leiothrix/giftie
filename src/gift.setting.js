30add310b22da0f3d1cada200de30833075b7a5f
Leiothrix <Leiothrix@users.noreply.github.com>
2023-02-13 14:57:40 +0800

    Update gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -19,9 +19,9 @@ export const homeConfig = {
     { key: 'd', wording: '然后，这里是第三行文案', },
   ],
   // 最终解释权归属人
-  owner: 'XXX',
+  owner: '转帖Bot小王',
   // 抽奖转盘的动画时间，单位毫秒
-  timeout: 5000,
+  timeout: 3000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
   targetGiftIndex: null,
 };
@@ -32,7 +32,7 @@ export const homeConfig = {
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
   { key: 'q', name: 'Dior双飞套装', alias: 'Dior双飞', image: '/images/1.png',  description: '「经典迷人色彩，彰显奢华魅力」' },
-  { key: 'w', name: '腾讯视频终身VIP', alias: 'VIP', image: '/images/2.png',  description: '“为你承包一辈子的 VIP”' },
+  { key: 'w', name: 'Bilibili终身大会员', alias: 'VIP', image: '/images/2.png',  description: '“为你承包一辈子的 VIP”' },
   { key: 'e', name: '兰蔻小黑瓶套装', alias: '小黑瓶套装', image: '/images/3.png',  description: '「强维稳，快修护」' },
   { key: 'r', name: '520红包', alias: '520', image: '/images/6.png',  description: '“想给你唱一百首情歌”' },
   { key: 't', name: 'Dior星空套装', alias: 'Dior星空', image: '/images/5.png',  description: '「百变唇妆，精美雕琢」' },
